752229e173a0005117d4699a166263034630b29f
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-02-09 13:13:27 -0700

    created files for repo w pseudocode

---
+++ Unsolved/script.js
@@ -0,0 +1,19 @@
+// //created in-class, 2/9/23
+
+// console.log(this);
+
+// // requests user input
+// var userChoice = prompt("Please choose Rock, Paper, or Scissors");
+
+// // send the user a message
+// var alert = "you have chosen" + userChoice;
+
+// var input = "returns boolean true/false";
+
+// BEGIN CODE
+// declaare variables
+// fxn declaration (main logic for game)
+// add prompt(r, p, s) [variables] first
+// alert (win/loss)
+
+var userChoice = prompt("Please choose R, P, or S...");
